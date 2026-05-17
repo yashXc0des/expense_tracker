160b30429cc145d42b42ba750069a1fe1095ffd5
YashXc0des <yashxcodes@gmail.com>
2026-05-17 23:56:26 +0530

    fix: login/signup

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'app/theme.dart';
 import 'features/home/home_screen.dart';
+import 'features/auth/login_screen.dart';
+import 'core/services/auth_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final prefs = await SharedPreferences.getInstance();
   final savedTheme = prefs.getString('themeMode') ?? 'system';
+  await AuthService.instance.init();
   runApp(MyApp(initialTheme: savedTheme));
 }
 
@@ -55,7 +58,44 @@ class _MyAppState extends State<MyApp> {
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: _themeMode,
-      home: HomeScreen(onThemeChanged: setTheme, themeMode: _themeMode),
+      home: AuthGate(onThemeChanged: setTheme, themeMode: _themeMode),
     );
+  }
+}
+
+class AuthGate extends StatefulWidget {
+  final Function(ThemeMode) onThemeChanged;
+  final ThemeMode themeMode;
+  const AuthGate({super.key, required this.onThemeChanged, required this.themeMode});
+
+  @override
+  State<AuthGate> createState() => _AuthGateState();
+}
+
+class _AuthGateState extends State<AuthGate> {
+  bool _checking = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _check();
+  }
+
+  Future<void> _check() async {
+    // AuthService already initialized in main
+    setState(() => _checking = false);
+  }
+
+  void _onLoggedIn() {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_checking) return const Scaffold(body: Center(child: CircularProgressIndicator()));
+    if (AuthService.instance.isLoggedIn) {
+      return HomeScreen(onThemeChanged: widget.onThemeChanged, themeMode: widget.themeMode);
+    }
+    return LoginScreen(onLoggedIn: _onLoggedIn);
   }
 }
